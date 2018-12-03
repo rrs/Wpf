947fadb9db1419c223941210125da0b4590060b2
rjsnipe <bobblezor@gmail.com>
2018-12-03 21:32:45 +0000

    added ViewModel to replace the verbosly name NotifyPropertyChangedViewModel, added current dispatcher helper

---
+++ src/Rrs.Wpf.Core/Invoker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace Rrs.Wpf.Core
+{
+    public static class Invoker
+    {
+        public static void BeginInvoke(Action a)
+        {
+            Application.Current.Dispatcher.BeginInvoke(a);
+        }
+    }
+}
