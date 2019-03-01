f5a1dc220b2a2665997fc10f3efe8c34a322ee65
Robert Snipe <bobblezor@gmail.com>
2019-03-01 22:36:49 +0000

    added some extra methods to Invoker.cs

--- src/Rrs.Wpf.Core/Invoker.cs
+++ src/Rrs.Wpf.Core/Invoker.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace Rrs.Wpf.Core
 {
     public static class Invoker
     {
-        public static void BeginInvoke(Action a)
+        public static void RequeryCommands()
         {
-            Application.Current.Dispatcher.BeginInvoke(a);
+            Application.Current.Dispatcher.Invoke((Action)CommandManager.InvalidateRequerySuggested);
+        }
+
+        public static DispatcherOperation BeginInvoke(Action a)
+        {
+            return Application.Current.Dispatcher.BeginInvoke(a);
+        }
+
+        public static void Invoke(Action action)
+        {
+            Application.Current.Dispatcher.Invoke(action);
+        }
+
+        public static T Invoke<T>(Func<T> func)
+        {
+            return (T)Application.Current.Dispatcher.Invoke(func);
         }
     }
 }
