c05019adfac63862aa45a78316950b547b948353
Robert Snipe <bobblezor@gmail.com>
2019-03-01 22:48:13 +0000

    added InvokeAsync methods to Invoker

--- src/Rrs.Wpf.Core/Invoker.cs
+++ src/Rrs.Wpf.Core/Invoker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
@@ -25,6 +26,44 @@ namespace Rrs.Wpf.Core
         public static T Invoke<T>(Func<T> func)
         {
             return (T)Application.Current.Dispatcher.Invoke(func);
+        }
+
+        public static Task<T> InvokeAsync<T>(this Dispatcher dispatcher, Func<T> f)
+        {
+            var tcs = new TaskCompletionSource<T>();
+            Application.Current.Dispatcher.BeginInvoke((Action)(() =>
+            {
+                try
+                {
+                    var v = f();
+                    tcs.SetResult(v);
+                }
+                catch (Exception e)
+                {
+                    tcs.SetException(e);
+                }
+            }));
+
+            return tcs.Task;
+        }
+
+        public static Task InvokeAsync(this Dispatcher dispatcher, Action a)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            Application.Current.Dispatcher.BeginInvoke((Action)(() =>
+            {
+                try
+                {
+                    a();
+                    tcs.SetResult(null);
+                }
+                catch (Exception e)
+                {
+                    tcs.SetException(e);
+                }
+            }));
+
+            return tcs.Task;
         }
     }
 }
