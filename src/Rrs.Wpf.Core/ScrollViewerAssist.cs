e628fb6a9ddf5f72ff13cd2a2667e552ee3a882c
rjsnipe <bobblezor@gmail.com>
2019-08-30 08:13:27 +0100

    added auto scroll AP

---
+++ src/Rrs.Wpf.Core/ScrollViewerAssist.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Rrs.Wpf.Core
+{
+    public static class ScrollViewerAssist
+    {
+        public static readonly DependencyProperty AutoScrollProperty = DependencyProperty.RegisterAttached("AutoScroll", typeof(bool), typeof(ScrollViewerAssist), new PropertyMetadata(false, AutoScrollPropertyChanged));
+
+        public static bool GetAutoScroll(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoScrollProperty);
+        }
+
+        public static void SetAutoScroll(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoScrollProperty, value);
+        }
+
+        public static void AutoScrollPropertyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            var scrollViewer = obj as ScrollViewer;
+            if (scrollViewer != null && (bool)args.NewValue)
+            {
+                scrollViewer.ScrollChanged += ScrollViewer_ScrollChanged;
+                scrollViewer.ScrollToEnd();
+            }
+            else
+            {
+                scrollViewer.ScrollChanged -= ScrollViewer_ScrollChanged;
+            }
+        }
+
+        private static void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            // Only scroll to bottom when the extent changed. Otherwise you can't scroll up
+            if (e.ExtentHeightChange != 0)
+            {
+                var scrollViewer = sender as ScrollViewer;
+                scrollViewer?.ScrollToBottom();
+            }
+        }
+
+    }
+}
