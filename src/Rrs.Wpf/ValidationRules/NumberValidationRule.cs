e5884fe94e5630739dd6eeb65926ea8af5d937e6
Rob Snipe <bobblezor@gmail.com>
2023-08-21 12:12:05 +0100

    added ability for number validation rule to work with ValidationSets that pass binding expressions

--- src/Rrs.Wpf/ValidationRules/NumberValidationRule.cs
+++ src/Rrs.Wpf/ValidationRules/NumberValidationRule.cs
@@ -1,5 +1,12 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Xml.Linq;
 
 namespace Rrs.Wpf.ValidationRules;
 
@@ -10,6 +17,12 @@ public class NumberValidationRule : ValidationRule
 
     public override ValidationResult Validate(object value, CultureInfo cultureInfo)
     {
+        if (value is BindingExpression binding)
+        {
+            //value = binding.ResolvedSource.GetType().GetProperty(binding.ResolvedSourcePropertyName).GetValue(binding.ResolvedSource);
+            value = GetterResolver.Get(binding.ResolvedSource, binding.ResolvedSourcePropertyName);
+        }
+        
         if (decimal.TryParse(value?.ToString(), out var d))
         {
             if (Min.HasValue && d < Min) return new ValidationResult(false, $"Value can not be less than {Min}");
@@ -25,4 +38,53 @@ public class NumberValidationRule : ValidationRule
             return new ValidationResult(false, $"Value is not a number");
         }
     }
+
+    private static class GetterResolver
+    {
+        private static readonly Dictionary<Type, Dictionary<string, Func<object, object>>> _getters = new();
+
+        public static object Get(object source, string propertyName)
+        {
+            var sourceType = source.GetType();
+            if (!_getters.ContainsKey(sourceType))
+            {
+                var properties = sourceType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                var getters = new Dictionary<string, Func<object, object>>();
+                foreach(var prop in properties)
+                {
+                    var parameterExpression = Expression.Parameter(typeof(object));
+                    var body = Expression.Convert(Expression.Property(Expression.Convert(parameterExpression, sourceType), propertyName), typeof(object));
+                    var lambda = Expression.Lambda<Func<object, object>>(body, parameterExpression);
+                    getters.Add(prop.Name, lambda.Compile());
+                }
+                //var getters = properties.ToDictionary(o => o.Name, o => (Func<object, object>)Delegate.CreateDelegate(typeof(Func<object, object>), o.GetGetMethod()));
+                _getters[sourceType] = getters;
+            }
+
+            return _getters[sourceType][propertyName](source);
+        }
+    }
+
+    //private interface IGetterResolver
+    //{
+
+    //}
+
+    //private class DelegateResolver<T>
+    //{
+    //    private static IDictionary<string, Func<T, object>> _propertyGetters;
+        
+    //    static DelegateResolver()
+    //    {
+    //        var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+    //        _propertyGetters = properties.ToDictionary(o => o.Name, o => (Func<T, object>)Delegate.CreateDelegate(typeof(Func<T, object>), o.GetGetMethod()));
+    //    }
+
+    //    public object Get(T source, string property)
+    //    {
+    //        return _propertyGetters[property](source);
+    //    }
+    //}
+
 }
