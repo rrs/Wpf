5fe50948d6a4e1b0dadcd31f31f39485bc568ba0
Rob Snipe <bobblezor@gmail.com>
2023-08-22 16:08:28 +0100

    added number format string for min max error

--- src/Rrs.Wpf/ValidationRules/NumberValidationRule.cs
+++ src/Rrs.Wpf/ValidationRules/NumberValidationRule.cs
@@ -15,28 +15,31 @@ public class NumberValidationRule : ValidationRule
     public decimal? Min { get; set; }
     public decimal? Max { get; set; }
 
+    public string ErrorNumberStringFormat { get; set; }
+
     public override ValidationResult Validate(object value, CultureInfo cultureInfo)
     {
         if (value is BindingExpression binding)
         {
-            //value = binding.ResolvedSource.GetType().GetProperty(binding.ResolvedSourcePropertyName).GetValue(binding.ResolvedSource);
             value = GetterResolver.Get(binding.ResolvedSource, binding.ResolvedSourcePropertyName);
         }
         
         if (decimal.TryParse(value?.ToString(), out var d))
         {
-            if (Min.HasValue && d < Min) return new ValidationResult(false, $"Value can not be less than {Min}");
-            if (Max.HasValue && d > Max) return new ValidationResult(false, $"Value can not be more than {Max}");
+            if (Min.HasValue && d < Min) return new ValidationResult(false, $"Value can not be less than {FormatNumber(Min.Value, ErrorNumberStringFormat)}");
+            if (Max.HasValue && d > Max) return new ValidationResult(false, $"Value can not be more than {FormatNumber(Max.Value, ErrorNumberStringFormat)}");
             return ValidationResult.ValidResult;
         }
         else if (value == null)
         {
-            return new ValidationResult(false, $"Value can not be nothing");
+            return new ValidationResult(false, $"Value is required");
         }
         else
         {
             return new ValidationResult(false, $"Value is not a number");
         }
+
+        static string FormatNumber(decimal value, string formatString) => string.IsNullOrWhiteSpace(formatString) ? value.ToString() : value.ToString(formatString);
     }
 
     private static class GetterResolver
@@ -57,34 +60,10 @@ public class NumberValidationRule : ValidationRule
                     var lambda = Expression.Lambda<Func<object, object>>(body, parameterExpression);
                     getters.Add(prop.Name, lambda.Compile());
                 }
-                //var getters = properties.ToDictionary(o => o.Name, o => (Func<object, object>)Delegate.CreateDelegate(typeof(Func<object, object>), o.GetGetMethod()));
                 _getters[sourceType] = getters;
             }
 
             return _getters[sourceType][propertyName](source);
         }
     }
-
-    //private interface IGetterResolver
-    //{
-
-    //}
-
-    //private class DelegateResolver<T>
-    //{
-    //    private static IDictionary<string, Func<T, object>> _propertyGetters;
-        
-    //    static DelegateResolver()
-    //    {
-    //        var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-    //        _propertyGetters = properties.ToDictionary(o => o.Name, o => (Func<T, object>)Delegate.CreateDelegate(typeof(Func<T, object>), o.GetGetMethod()));
-    //    }
-
-    //    public object Get(T source, string property)
-    //    {
-    //        return _propertyGetters[property](source);
-    //    }
-    //}
-
 }
