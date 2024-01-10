389ae9a5110726dc47383d22dad4ea6011db43b5
Rob Snipe <bobblezor@gmail.com>
2024-01-10 11:20:19 +0000

    added ownit extension

---
+++ src/Rrs.Wpf/WindowExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Rrs.Wpf;
+
+public static class WindowExtensions
+{
+    public static T OwnIt<T>(this T w, IntPtr ownerHandle) where T : Window
+    {
+        if (ownerHandle == IntPtr.Zero) return w;
+        new WindowInteropHelper(w)
+        {
+            Owner = ownerHandle
+        };
+        return w;
+    }
+}
